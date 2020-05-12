900d3ce24ef8b4520defe7254d94a848179a023c
Chronic Coder <efeoghene.obebeduo@gmail.com>
2020-05-12 02:20:58 +0100

    Replace LocomotionControl with Wheels for simplicity

--- dubois/duboisd.py
+++ dubois/duboisd.py
@@ -2,13 +2,13 @@
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, disconnect
-from controls.locomotion import LocomotionControl
+from transducers.output import Wheels
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=None)
 
-locomotionControl = LocomotionControl()
+wheels = Wheels()
 
 @app.route('/')
 def index():
@@ -22,38 +22,38 @@ def test_message(message):
 def move_forward(message):
     print('Move forward', message)
     if message['activated']:
-        locomotionControl.move_forward()
+        wheels.move_forward()
     else:
-        locomotionControl.stop()
+        wheels.stop()
 
 @socketio.on('backward', namespace='/test')
 def move_backward(message):
     print('Move backward', message)
     if message['activated']:
-        locomotionControl.move_in_reverse()
+        wheels.move_in_reverse()
     else:
-        locomotionControl.stop()
+        wheels.stop()
 
 @socketio.on('clockwise', namespace='/test')
 def move_clockwise(message):
     print('Move clockwise', message)
     if message['activated']:
         pass
-        locomotionControl.move_clockwise()
+        wheels.move_clockwise()
     else:
-        locomotionControl.stop()
+        wheels.stop()
 
 @socketio.on('counterclockwise', namespace='/test')
 def move_counterclockwise(message):
     print('Move counterclockwise', message)
     if message['activated']:
         pass
-        locomotionControl.move_counterclockwise()
+        wheels.move_counterclockwise()
     else:
-        locomotionControl.stop()
+        wheels.stop()
 
 if __name__ == '__main__':
     try:
         socketio.run(app, debug=True, host='0.0.0.0', port=5000)
     except KeyboardInterrupt:
-        locomotionControl.shutdown()
+        wheels.shutdown()
